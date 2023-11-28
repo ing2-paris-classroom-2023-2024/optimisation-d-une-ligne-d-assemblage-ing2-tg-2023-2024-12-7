6673c2f076f50c994ec161f30e0bca10e727f300
QuentinLCS78 <119486216+QuentinLCS78@users.noreply.github.com>
2023-11-28 17:30:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -42,7 +42,50 @@ struct Station NewStation()
     struct Station New;
     New.temps = 0;
     New.nmbOp = 0;
-    New.couleur = 0;
+    New.couleur = 0;   
 
     return New;
+}
+void CompterAnt(struct Operation AllOp[100],int nmb)
+{
+    for(int i = 0 ; i < nmb ; i++)
+    {
+        for(int j = 0 ; j < nmb ; j++)
+        {
+            for(int l = 0 ; l < AllOp[j].nmbPr ; l++)
+            {
+                if(AllOp[j].prec[l]->num == AllOp[i].num)
+                {
+                    AllOp[i].nmbAnt++;
+                }
+            }
+        }
+    }
+}
+
+void ajouterOperation(struct Station * _station,struct Operation _operation)
+{
+    _station->temps += _operation.temps;
+    _station->Op[_station->nmbOp] = _operation;
+    _station->nmbOp++;
+}
+
+void afficherStation(struct Station  _station)
+{
+    printf("Liste des operations: ");
+    for(int i = 0 ; i < _station.nmbOp ; i++)
+    {
+        printf("%d ",_station.Op[i].num);
+    }
+    printf("\nTemps total d'execution: %f\n",_station.temps);
+}
+
+int existe(struct Operation AllOp[100],int nmb, int num)
+{
+    for(int i = 0 ; i <nmb ; i++)
+    {
+        if(AllOp[i].num == num)
+            return i;
+    }
+    return 200;
 }
