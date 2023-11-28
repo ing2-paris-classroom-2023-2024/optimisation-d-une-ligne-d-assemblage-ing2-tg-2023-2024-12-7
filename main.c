3ac7f3abb25951a039c80725e4d2816760bf98d6
QuentinLCS78 <119486216+QuentinLCS78@users.noreply.github.com>
2023-11-28 18:07:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+struct Operation
+{
+    int num;
+    struct Operation *exclu[10];
+    int nmbEx;
+    struct Operation *prec[10];
+    int nmbPr;
+    float temps;
+    int nmbAnt;
+    int couleurWelsh;
+    int triTopo;
+};
+
+
+struct Station
+{
+    float temps;
+    struct Operation Op[50];
+    int nmbOp;
+    int couleur;
+};
+
+struct Operation newOp(int _num)
+{
+    struct Operation New;
+    New.num = _num;
+    New.nmbEx = 0;
+    New.temps = 0;
+    New.nmbPr = 0;
+    New.nmbAnt = 0;
+    New.couleurWelsh = -1;
+
+    return New;
+}
